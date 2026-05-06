7bb49c6aae95a8b3cde305511560681792d43260
TheRealMido <therealmido349@gmail.com>
2026-05-06 21:32:00 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mind-mate-final-edition/sw.js', { scope: '/mind-mate-final-edition/' })})}
